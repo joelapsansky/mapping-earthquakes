a71161726b0ab824e5f19f1995391a4b12152167
Joe Lapsansky <joe.lapsansky@gmail.com>
2021-03-07 22:22:10 -0600

    adding updated polygon files

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -1,10 +1,10 @@
-// Add console.log to check to see if our code is working.
-// console.log("working");
+// Add console.log to check to see if our code is working
+console.log("working");
 
-// Create the map object with a center and zoom level.
+// Create the map object with a center and zoom level
 let map = L.map('mapid').setView([37.5, -122.5], 10);
 
-// Add GeoJSON data.
+// Add GeoJSON data
 let sanFranAirport =
 {"type":"FeatureCollection","features":[{
     "type":"Feature",
@@ -25,9 +25,9 @@ let sanFranAirport =
 ]};
 
 
-// Grabbing our GeoJSON data.
+// Grabbing our GeoJSON data
 L.geoJson(sanFranAirport, {
-    // We turn each feature into a marker on the map.
+    // We turn each feature into a marker on the map
     onEachFeature: function(feature, layer) {
       console.log(layer);
       layer.bindPopup("Ariport Code: " + feature.properties.faa + 
@@ -51,11 +51,11 @@ L.geoJson(sanFranAirport, {
 //     .addTo(map);
 // });
 
-// We create the tile layer that will be the background of our map.
+// We create the tile layer that will be the background of our map
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
-// Then we add our 'graymap' tile layer to the map.
+// Then we add our 'graymap' tile layer to the map
 streets.addTo(map);
